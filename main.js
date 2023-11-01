3ff9db09f88a67a546636c4466b8c1293ba7241d
Nish <nishxpatel@gmail.com>
2023-11-01 16:51:46 -0400

    functionality and visual enhancements

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
+const SIZE = 12;
 
-const SIZE = 10;
-
-
-
-let isDragging = false;
 const numRows = SIZE;
-const numCols  = SIZE;
+const numCols = SIZE;
+let isDragging = false;
+let currentMode = 'draw';
 
 const table = document.getElementById('grid');
 
@@ -13,13 +11,23 @@ for (let i = 0; i < numRows; i++) {
     const row = document.createElement('tr');
     for (let j = 0; j < numCols; j++) {
         const cell = document.createElement('td');
+
+        // Add event listeners for drag coloring/erasing
         cell.addEventListener('mousedown', function() {
             isDragging = true;
-            cell.style.backgroundColor = 'red';
+            if (currentMode === 'draw') {
+                cell.style.backgroundColor = 'black';
+            } else if (currentMode === 'erase') {
+                cell.style.backgroundColor = '';
+            }
         });
         cell.addEventListener('mouseover', function() {
             if (isDragging) {
-                cell.style.backgroundColor = 'red';
+                if (currentMode === 'draw') {
+                    cell.style.backgroundColor = 'black';
+                } else if (currentMode === 'erase') {
+                    cell.style.backgroundColor = '';
+                }
             }
         });
         cell.addEventListener('mouseup', function() {
@@ -30,3 +38,35 @@ for (let i = 0; i < numRows; i++) {
     }
     table.appendChild(row);
 }
+
+// Reset isDragging if mouseup is detected outside the table cells
+document.addEventListener('mouseup', function() {
+    isDragging = false;
+});
+
+document.getElementById('draw').addEventListener('click', function() {
+    currentMode = 'draw';
+    updateButtonStates();
+});
+document.getElementById('eraser').addEventListener('click', function() {
+    currentMode = 'erase';
+    updateButtonStates();
+});
+document.getElementById('clear').addEventListener('click', function() {
+    const cells = document.querySelectorAll('#grid td');
+    cells.forEach(cell => cell.style.backgroundColor = '');
+});
+
+
+function updateButtonStates() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.classList.remove('active'));
+
+    if (currentMode === 'draw') {
+        document.getElementById('draw').classList.add('active');
+    } else if (currentMode === 'erase') {
+        document.getElementById('eraser').classList.add('active');
+    }
+}
+
+updateButtonStates();
